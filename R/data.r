211f0afd3429410a592d3d3ad37e646f358599f6
Heike Hofmann <hofmann@iastate.edu>
2013-09-02 17:46:30 -0500

    data documentation

---
+++ R/data.r
@@ -0,0 +1,26 @@
+#' Map of congressional districts based on the 113th congress
+#' 
+#' @name cdmap
+#' @title congressional districts
+#' @description Map of the Congressional districts as determined by the 113th congress, downloaded from http://www.census.gov/cgi-bin/geo/shapefilesrd13/layers.cgi and modeified for use as map object in ggplot2.
+#' @docType data
+#' @usage data(cdmap)
+NULL
+
+#' Census Bureau Datafiles
+#' 
+#' @name censusData
+#' @title Census Bureau Database information
+#' @description Information on all (as of Aug 2013) databases available for access through the Census Bureau's API at http://www.census.gov/developers/data/
+#' @docType data
+#' @usage data(censusData)
+NULL
+
+#' key
+#' 
+#' @name key
+#' @title key for access to the Census Bureau's API
+#' @description key for access to the Census Bureau's API, available from http://www.census.gov/developers/tos/key_request.html
+#' @docType data
+#' @usage data(key)
+NULL
